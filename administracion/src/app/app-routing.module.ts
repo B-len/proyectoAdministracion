01d942abff7f312b527abeb203329315a3f2c374
B-len <mary.belencatorce@gmail.com>
2019-03-25 00:22:57 +0100

    Rutas básicas, css en home y enlace a registro

--- administracion/src/app/app-routing.module.ts
+++ administracion/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { InicioSesionComponent} from "./inicio-sesion/inicio-sesion.component";
+import { HomeComponent} from "./home/home.component";
+
+const routes: Routes = [
+  {path:'', redirectTo:'home', pathMatch:'full'},
+  {path:'home', component:HomeComponent},
+  {path: 'inicioSesion', component: InicioSesionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
